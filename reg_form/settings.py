d39224304ba0bd2c17a6789c534d884b3a24cad7
Hareesh Bashyam <bashyamhareesh@gmail.com>
2025-09-28 11:48:36 +0530

    Update settings.py

--- reg_form/settings.py
+++ reg_form/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'practice',
         'USER':'root',
-        'PASSWORD':'Hareesh'
+        'PASSWORD':'Hareesh',
         'HOST': 'localhost',
         'PORT': '3306',
     }
